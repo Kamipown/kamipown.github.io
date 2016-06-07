ae608f6016257133fbab5db56dbdfbd32688e591
Kamipown <paul.delobbel@gmail.com>
2016-06-07 22:55:44 +0200

    test mobil

--- scripts/app.js
+++ scripts/app.js
@@ -5,3 +5,12 @@ var app = angular.module("app", [/*"ngRoute"*/]);
 // 	$routeProvider.when("/", {templateUrl: "partials/home.html", controller: "home_ctrl"});
 // 	$routeProvider.otherwise({templateUrl: "partials/notfound.html", controller: "notfound_ctrl"});
 // });
+
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))
+{
+    document.getElementById("main_header").style.backgroundColor = "#55c";
+}
+else
+{
+    document.getElementById("main_header").style.backgroundColor = "#c55";
+}
