0bc89367533606f7d69c619af6ff25bc8d8dcd77
Kamipown <paul.delobbel@gmail.com>
2016-06-07 23:10:46 +0200

    end test

--- scripts/app.js
+++ scripts/app.js
@@ -5,12 +5,3 @@ var app = angular.module("app", [/*"ngRoute"*/]);
 // 	$routeProvider.when("/", {templateUrl: "partials/home.html", controller: "home_ctrl"});
 // 	$routeProvider.otherwise({templateUrl: "partials/notfound.html", controller: "notfound_ctrl"});
 // });
-
-if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))
-{
-    document.getElementById("main_header").style.backgroundColor = "#55c";
-}
-else
-{
-    document.getElementById("main_header").style.backgroundColor = "#c55";
-}
