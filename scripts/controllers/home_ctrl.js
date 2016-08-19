6dc5ecd81289fa0cb88f02adfef6b1e985b028e1
Kamipown <paul.delobbel@gmail.com>
2016-08-19 06:09:25 +0200

    gh-pages

--- scripts/controllers/home_ctrl.js
+++ scripts/controllers/home_ctrl.js
@@ -1,4 +1,15 @@
-app.controller("home_ctrl", function($scope)
+app.controller("home_ctrl", function($scope, $timeout)
 {
-
+	$scope.init = function()
+	{
+		$timeout(function()
+		{
+			document.getElementById("cpp_progress").style.width = "80%";
+			document.getElementById("c_progress").style.width = "99%";
+			document.getElementById("js_progress").style.width = "75%";
+			document.getElementById("unity_progress").style.width = "50%";
+			document.getElementById("ue4_progress").style.width = "40%";
+			document.getElementById("sdl2_progress").style.width = "85%";
+		}, 500);
+	}
 });
