a672be072254e6915eaa05e2b26eea1daaae4683
kamipown <paul.delobbel@gmail.com>
2019-08-29 06:44:05 +0200

    add experiences

--- rcs/js/app.js
+++ rcs/js/app.js
@@ -1,3 +1,24 @@
+moment.locale("fr");
+
 window.addEventListener("load", function() {
+	document.getElementById("age").innerHTML = moment().diff("1994-06-20", 'y');
 	document.body.style.opacity = 1;
 }, false);
+
+var Ui = {
+	galadrimContent: false,
+	cremedelacremeContent: false,
+	cimesContent: false,
+
+	toggleExperience: function(element, content) {
+		this[content] = !this[content];
+		if (this[content]) {
+			document.getElementById(content).classList.add("active");
+			element.value = "RÉDUIRE";
+		}
+		else {
+			document.getElementById(content).classList.remove("active");
+			element.value = "EN SAVOIR PLUS";
+		}
+	}
+};
